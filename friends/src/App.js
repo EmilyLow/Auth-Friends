2242640fd18692d33847bd4ac09d8f6a8a7672a9
EmilyLow <emlow@me.com>
2020-09-04 12:20:15 -0500

    Router works

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,20 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import FriendsList from "./components/FriendsList";
 import LoginForm from "./components/LoginForm";
 
 function App() {
-  return (
-    <div className="App">
+  // return (
+  //   <div className="App">
     
-        <p>
-          Friends App
-        </p>
+  //       <p>
+  //         Friends App
+  //       </p>
       
-      <LoginForm/>
-      <FriendsList/>
-    </div>
+  //     <LoginForm/>
+  //     <FriendsList/>
+  //   </div>
+  // );
+
+  return (
+    <Router>
+      <div classname="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/protected" component={FriendsList}/>
+          <Route path="/login" component={LoginForm}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
