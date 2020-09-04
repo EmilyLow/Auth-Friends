7ac840c9d8f80298f2aa42102cace5c75d61429d
EmilyLow <emlow@me.com>
2020-09-04 14:20:09 -0500

    Working friends list

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,6 +1,7 @@
 
 
 import React from 'react';
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 
 class FriendsList extends React.Component {
@@ -17,11 +18,30 @@ class FriendsList extends React.Component {
 
         //Call axiosWithAuth to get actual friends data and then set it to state
 
+        axiosWithAuth()
+            .get("/friends")
+            .then((res) => {
+                // console.log("Friend data", res)
+                this.setState({
+                    ...this.state,
+                    friends: res.data
+                })
+                console.log("Friends",this.state.friends);
+            })
+            .catch((err) => console.log("getData error:", err));
+
     };
 
+    //Need to add loading
     render () {
         return (
-            <p>FriendsList</p>
+          <div>
+            <p>Friends List</p>
+            {this.state.friends.map(friend => (
+                <p>A new friend</p>
+            ))}
+
+          </div>
         )
     }
     
